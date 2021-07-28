ee3d79749aee98acbf05670eb356831ce593eb90
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-28 16:45:18 -0400

    Updates

---
+++ precache-manifest.04737bf0e10a688b1d2c535dab859005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6dca45326d8a574408557bb58ab8d5",
+    "url": "/reactQuizGame/index.html"
+  },
+  {
+    "revision": "67208b8e79b4559db079",
+    "url": "/reactQuizGame/static/css/main.50566d9f.chunk.css"
+  },
+  {
+    "revision": "e819485e4ab338fed58c",
+    "url": "/reactQuizGame/static/js/2.c05d0765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactQuizGame/static/js/2.c05d0765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67208b8e79b4559db079",
+    "url": "/reactQuizGame/static/js/main.8958ee4c.chunk.js"
+  },
+  {
+    "revision": "d5a52db24b7392dbf324",
+    "url": "/reactQuizGame/static/js/runtime-main.7f2cc9e4.js"
+  }
+]);
