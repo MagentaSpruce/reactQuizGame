88947d3bcf49b268feab9ef6069751a6b27b672b
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-28 14:37:05 -0400

    Show modal
    openModal and closeModal functions created and used. Modal component built out.

--- src/context.js
+++ src/context.js
@@ -50,8 +50,8 @@ const AppProvider = ({ children }) => {
     setIndex((oldIndex) => {
       const index = oldIndex + 1;
       if (index > questions.length - 1) {
+        openModal();
         return 0;
-        //open modal
       } else {
         return index;
       }
@@ -63,6 +63,16 @@ const AppProvider = ({ children }) => {
       setCorrect((oldState) => oldState + 1);
     }
     nextQuestion();
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setWaiting(true);
+    setCorrect(0);
+    setIsModalOpen(false);
   };
 
   useEffect(() => {
@@ -81,6 +91,7 @@ const AppProvider = ({ children }) => {
         isModalOpen,
         nextQuestion,
         checkAnswer,
+        closeModal,
       }}
     >
       {children}
